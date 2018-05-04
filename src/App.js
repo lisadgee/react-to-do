afd518cc5b883a2d494d5368a85cf2173754535b
Lisa Gee <lisadgee@hotmail.com>
2018-05-03 21:21:05 -0400

    complete events

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import ToDo from './components/ToDo.js';
 // class ToDo extends Component {
 //     render() {
 //       return (
-//         <li> A todo will go here </li>
+//         <li> 
+//             <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+//           </li>
 //       );
 //     }
 // }
-//export default ToDo;
+
 
 
 class App extends Component {
@@ -20,8 +22,33 @@ class App extends Component {
         { description: 'Walk the cat', isCompleted: true },
         { description: 'Throw the dishes away', isCompleted: false },
         { description: 'Buy new dishes', isCompleted: false }
-      ]
+      ],
+      newTodoDescription: ''
+     
     };
+  }
+
+  deleteTodo(index){
+    const todos = this.state.todos.filter((currentVal, i)=> i!==index );    
+    this.setState({ todos: todos });
+  }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });  
+  }
+
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
   }
   
   render() {
@@ -29,14 +56,20 @@ class App extends Component {
       <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) => 
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ ()=> this.deleteTodo(index)}/>
           )}
         </ul>
+        
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+        <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+           <input type="submit" />
+         </form>
       </div>
     );
   }
 }
-         
+    
+                        
 
 
 export default App;
